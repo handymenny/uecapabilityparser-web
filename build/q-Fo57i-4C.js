bb6a0ed13b1c622b037c40e129f99c01cc0935bf
HandyMenny <HandyMenny@users.noreply.github.com>
2023-12-18 18:03:04 +0000

    deploy: 69e51fd908ce957c6b584c53ab275af1ac84223a

---
+++ build/q-Fo57i-4C.js
@@ -0,0 +1,7 @@
+import{c as n,o as e,k as t,P as m,N as p,g as C,_ as R}from"./q-OIKTu0zg.js";import{c as f,C as g,d as x,D as y,R as h,e as P,f as E,h as I,i as b}from"./q-eWPsPYVU.js";const w=async o=>{throw new Error("Not implemented")},N=o=>{const s=o.url??"http://localhost/",a=new URL(s),r=n({url:a,params:o.params??{},isNavigating:!1,prevUrl:void 0},{deep:!1}),c=e({}),i=e({type:"initial",dest:a}),l=C(()=>R(()=>Promise.resolve().then(()=>S),void 0),"s_BUbtvTyvVRE"),d=n(f,{deep:!1}),u=n({headings:void 0,menu:void 0},{deep:!1}),_=e(),v=e();return t(g,u),t(x,_),t(y,d),t(h,r),t(P,l),t(E,c),t(I,v),t(b,i),m(p,null,3,"qY_1")},S=Object.freeze(Object.defineProperty({__proto__:null,s_BUbtvTyvVRE:w,s_WmYC5H00wtI:N},Symbol.toStringTag,{value:"Module"}));export{w as s_BUbtvTyvVRE,N as s_WmYC5H00wtI};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = []
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
