90fae1614d949a2ab957086885032c4850fcb51f
handymenny <handymenny@outlook.com>
2024-01-11 22:22:02 +0100

    circle spinner add padding

--- src/components/spinner/circle-spinner.tsx
+++ src/components/spinner/circle-spinner.tsx
@@ -5,5 +5,5 @@ export default component$(({ centered }: { centered?: boolean }) => {
     <div class="h-64 w-64 animate-spin rounded-full border-[12px] border-solid border-gray-100 border-t-gray-800"></div>
   );
 
-  return centered ? <div class="m-auto">{spinner}</div> : spinner;
+  return centered ? <div class="m-auto p-12">{spinner}</div> : spinner;
 });
